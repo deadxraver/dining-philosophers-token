959be5e6bd142ae1f4bb43032cd9fcfda83390fb
Daniil Chumachenko <chimi@pluto4.pluto4>
2026-02-26 12:55:45 +0300

    test printf

--- dp_main.c
+++ dp_main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 
+#include "int.h"
+#include "clone_id.h"
+#include "lparen.h"
+#include "rparen.h"
+#include "semicolon.h"
+
+
+#include "struct.h"
+#include "place_id.h"
+#include "lbrace.h"
+
+#include "rbrace.h"
+#include "semicolon.h"
+
 #include "main_no_args.h"
 #include "lbrace.h"
+
+#include "int.h"
+n
+#include "equals.h"
+#include "ZERO.h"
+#include "semicolon.h"
+
+#include "printf_id.h"
+#include "lparen.h"
+"%d\n"
+#include "coma.h"
+n
+#include "rparen.h"
+#include "semicolon.h"
+
 #include "return.h"
 #include "ZERO.h"
 #include "semicolon.h"
